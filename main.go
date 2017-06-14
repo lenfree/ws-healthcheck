4c6696234ca4fb9676f9c00aca48d31e4f032803
Lenfree Yeung <lenfree.yeung@gmail.com>
2017-06-14 10:31:33 +1000

    send a ping

--- main.go
+++ main.go
@@ -8,25 +8,31 @@ import (
 	"golang.org/x/net/websocket"
 )
 
+type message struct {
+	Action string
+}
+
 // Thanks to https://gowalker.org/golang.org/x/net/websocket#_ex_btn_Dial
 func main() {
 	host := os.Getenv("HOST")
 
-	origin := "http://" + os.Getenv("ORIGIN")
+	//origin := "http://" + os.Getenv("ORIGIN")
+	origin := "http://localhost/"
 	url := "ws://" + host + ":" + os.Getenv("PORT") + "/ws/ping"
 
-	fmt.Printf("url: %s", url)
+	fmt.Printf("url: %s\n", url)
 	ws, err := websocket.Dial(url, "", origin)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if _, err := ws.Write([]byte("hello, world!\n")); err != nil {
+
+	msg := message{Action: "ping"}
+	fmt.Println("writing to ws")
+	if err := websocket.JSON.Send(ws, msg); err != nil {
 		log.Fatal(err)
 	}
-	var msg = make([]byte, 512)
-	var n int
-	if n, err = ws.Read(msg); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("Received: %s.\n", msg[:n])
+
+	data := message{}
+	websocket.JSON.Receive(ws, &data)
+	fmt.Printf("received: %#v\n", data)
 }
